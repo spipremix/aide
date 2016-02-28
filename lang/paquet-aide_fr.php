3f1841bea18ecb4cd7172fd16c690ec45f999b42
eric <eric@smellup.net>
2016-02-28 10:16:45 +0000

    Première mise à jour du plugin Aide. Pour l'instant aucun changement de méthode, juste une séparation des fichiers avec le core. Le core conserve une API constituée de la balise #AIDER et de la fonction aide() pour le PHP.

---
+++ lang/paquet-aide_fr.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aide_description' => 'Ce plugin permet d\'inclure dans SPIP une aide contextuelle repérée par un icone. Cette aide peut-être étendue aussi aux plugins.',
+	'aide_nom' => 'Aide SPIP',
+	'aide_slogan' => 'Aide en ligne de SPIP'
+);
+
+?>
